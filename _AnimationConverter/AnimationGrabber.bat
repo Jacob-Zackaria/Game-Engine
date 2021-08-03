bd1e5cae1182e8221d21735150b42b544bab073c
Jacob Zackaria <44209093+Jacob-Zackaria@users.noreply.github.com>
2021-08-03 10:57:40 -0500

    Add files via upload

---
+++ _AnimationConverter/AnimationGrabber.bat
@@ -0,0 +1,15 @@
+REM 
+REM	create game engine format animations from models folder models.
+REM
+
+
+REM change directory
+
+	cd Debug
+REM call ImportScene.exe
+REM ..\Models\humanoid.fbx ..\Models\Frames2.clip ..\Models\Hierarchy2.skel
+REM ..\Models\teddy_TGA.fbx ..\Models\Frames4.clip ..\Models\Hierarchy4.skel
+
+ImportScene ..\\..\\Models\\humanoid.fbx ..\\..\\Models\\Frames2.clip ..\\..\\Models\\Hierarchy2.skel
+
+ImportScene ..\\..\\Models\\teddy_TGA.fbx ..\\..\\Models\\Frames4.clip ..\\..\\Models\\Hierarchy4.skel
